32e47c42f7510ad060260d8ceff742ea00737ea0
Keith Chester <kchester@gmail.com>
2015-11-05 10:13:16 -0600

    feature: reset and stop commands

--- jkaraoke.js
+++ jkaraoke.js
@@ -3,15 +3,15 @@
 function Karaoke(opts){
 	this.target = opts.target;
 	this.lyrics = opts.lyrics;
-	this.unHighlightedColor = opts.unHighlightedColor; 
+	this.unHighlightedColor = opts.unHighlightedColor;
 	this.highlightedColor = opts.highlightedColor;
-	
+
 	this._startedAt = 0;
 	this._lyricPointer = 0;
 	this._highlightPointer = 0;
 	this._highlightedWords = [];
 	this._unHighlightedWords = []
-	
+
 	this._lyricTimeout = null;
 	this._highlightTimeout = null;
 };
@@ -19,54 +19,69 @@ function Karaoke(opts){
 Karaoke.prototype.start = function(){
 	this._startedAt = (new Date()).getTime();
 	this._showCurrentLyrics();
+	this._stopFlag = false;
+};
+
+Karaoke.prototype.reset = function(){
+	this._lyricPointer = 0;
+	this._highlightPointer = 0;
+	this._highlightedWords = [];
+	this._unHighlightedWords = []
+
+	this._lyricTimeout = null;
+	this._highlightTimeout = null;
 };
 
 Karaoke.prototype._showNextLyric = function(){
 	var self = this;
-	
+
+	if(self._stopFlag) return;
+
 	if(++self._lyricPointer >= self.lyrics.length)
 		return self.stop();
-	
+
 	//Reset pointers
 	self._highlightPointer = 0;
-	
+
 	//Clear out arrs
 	self._highlightedWords = [];
 	self._unHighlightedWords = [];
-	
+
 	//Clear timeouts
 	if(self._lyricTimeout) clearTimeout(self._lyricTimeout);
 	if(self._highlightTimeout) clearTimeout(self._highlightTimeout);
-	
+
 	self._clearText();
 	self._showCurrentLyrics();
 };
 
 Karaoke.prototype._showCurrentLyrics = function(){
 	var self = this;
-	
+
 	if(self.lyrics[self._lyricPointer].type == "delay"){
 		self._clearText();
 	} else {
 		self.lyrics[self._lyricPointer].lyrics.forEach(function(lyric){
 			self._unHighlightedWords.push(lyric);
 		});
-		
+
 		self._setNextHighlight(self);
 	}
-	
+
 	self._lyricTimeout = setTimeout(
 		function(){
 			self._showNextLyric(self)
 		},
 		self.lyrics[self._lyricPointer].showFor
 	);
-	
+
 	self._setText(self._generateHTML());
 };
 
 Karaoke.prototype._setNextHighlight = function(){
 	var self = this;
+
+	if(self._stopFlag) return;
 
 	self._highlightTimeout = setTimeout(
 		function(){
@@ -76,9 +91,7 @@ Karaoke.prototype._setNextHighlight = function(){
 			self._setText(self._generateHTML(self));
 			if(self._highlightPointer < self.lyrics[self._lyricPointer].lyrics.length){
 				self._setNextHighlight(self);
-			}// else {
-			//	self._showNextLyric(self);
-			//}
+			}
 		},
 		self.lyrics[self._lyricPointer]
 				.lyrics[self._highlightPointer]
@@ -87,7 +100,7 @@ Karaoke.prototype._setNextHighlight = function(){
 };
 
 Karaoke.prototype.stop = function(){
-	
+	this._stopFlag = true;
 };
 
 Karaoke.prototype._generateHTML = function(){
@@ -96,25 +109,25 @@ Karaoke.prototype._generateHTML = function(){
 	if(this.highlightedColor) highlightedSpan += " style='color: " +
 		this.highlightedColor + "' ";
 	highlightedSpan += ">"
-	
+
 	this._highlightedWords.forEach(function(word){
 		highlightedSpan += word.lyric + " ";
 	});
-	
+
 	highlightedSpan += "</span>";
-	
+
 	//Unhighlighted next
 	var unHighlightedSpan = "<span class='karaokeUnHightlighted'";
 	if(this.unHighlightedColor) unHighlightedSpan += " style='color: " +
 		this.unHighlightedColor + "' ";
 	unHighlightedSpan += ">"
-	
+
 	this._unHighlightedWords.forEach(function(word){
 		unHighlightedSpan += word.lyric + " ";
 	});
-	
+
 	unHighlightedSpan += "</span>";
-	
+
 	return highlightedSpan + unHighlightedSpan;
 };
 
